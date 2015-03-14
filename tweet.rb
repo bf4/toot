0ac47a9ff8a289f2f45fba0eea177a7a899486f2
Benjamin Fleischer <github@benjaminfleischer.com>
2015-03-14 09:16:10 -0500

    Values

--- tweet.rb
+++ tweet.rb
@@ -1,11 +1,13 @@
-class Tweet
-  attr_reader :username, :text
-  def initialize(line, username, text)
-    @line = line
-    @username = username
-    @text = text
+require "values"
+
+class Tweet < Value.new(:tid, :username, :text)
+  def initialize(tid, username, text)
+    super(tid.to_i, username, text)
   end
 
+  def line_count
+    lines.count
+  end
 
   def lines
     @text.split("\n")

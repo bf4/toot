13264e7d3fcdc4cabb5f1908e51248e821290be3
Benjamin Fleischer <github@benjaminfleischer.com>
2015-03-14 09:00:12 -0500

    erlang

--- toot.rb
+++ toot.rb
@@ -2,12 +2,17 @@ class User < Struct.new(:username, :private)
   alias_method :private?, :private
 end
 
+# Fancy conditional tricks (Perl)
+# Ad-hoc polymorphism (Java)
+# Pattern Matching (Erlang)
+#
+
 class Follow
+  def self.follow(user | user.private?)
+    actually_follow(user) if confirm_follow(user)
+  end
+
   def self.follow(user)
-    if user.private?
-      actually_follow(user) if confirm_follow(user)
-    else
-      actually_follow(user)
-    end
+    actually_follow(user)
   end
 end
